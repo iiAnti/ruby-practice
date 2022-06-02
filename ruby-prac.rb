aab2128fc2539bdcd772cb73aababd56ace65922
Ivan O <trikshotzz05@gmail.com>
2022-06-02 01:18:09 -0700

    Update ruby-prac.rb

--- ruby-prac.rb
+++ ruby-prac.rb
@@ -22,4 +22,6 @@ my_adj3 = gets.chomp
 
 
 p "if you go to the bar and #{my_verb} you will get hit. I once 
-#{my_noun} did something like that. If anyone #{my_adj} then I will love you forever. There is alos this girl who is so #{my_adj2} and every time she #{my_verb2} I vant stop #{my_verb3}ing. Is that a little #{my_noun2}? can you describe what a #{my_adj3} is? "
+#{my_noun} did something like that. If anyone #{my_adj} then I will love you forever. 
+There is alos this girl who is so #{my_adj2} and every time she #{my_verb2} I vant stop #{my_verb3}ing.
+Is that a little #{my_noun2}? can you describe what a #{my_adj3} is? "
